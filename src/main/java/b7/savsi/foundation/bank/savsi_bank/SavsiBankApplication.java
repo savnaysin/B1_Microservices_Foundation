c9b609425b36fc18baacb47d41f64c8b50997148
Singh <a1048989@bestbuy.com>
2019-08-15 06:15:45 -0500

    first

---
+++ src/main/java/b7/savsi/foundation/bank/savsi_bank/SavsiBankApplication.java
@@ -0,0 +1,13 @@
+package b7.savsi.foundation.bank.savsi_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavsiBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavsiBankApplication.class, args);
+	}
+
+}
