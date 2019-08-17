b2931dac1a12244d76333e43d4aa5139d65ed47c
Singh <a1048989@bestbuy.com>
2019-08-16 23:42:43 -0500

    structure-2

--- src/main/java/b7/savsi/foundation/bank/savsi_bank/dao/AccountDao.java
+++ src/main/java/b7/savsi/foundation/bank/savsi_bank/dao/AccountDao.java
@@ -2,25 +2,13 @@ package b7.savsi.foundation.bank.savsi_bank.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import b7.savsi.foundation.bank.savsi_bank.entity.Account;
-import b7.savsi.foundation.bank.savsi_bank.entity.Customer;
 
-public class AccountDao {
-	
-	
-	public String saveAccount(Account Account) {
-		
-		
-		 return "";
-	}
+public interface AccountDao extends JpaRepository<Account, Integer> {
 
-	
-	public List<Account> findAllAccounts(){
-		
-	}
-	
-	public String saveCustomer (Customer customer) {
-		retunr "";
-	}
-	
+	String saveAccount(Account Account);
+
+	List<Account> findAll();
+
 }
--- src/main/java/b7/savsi/foundation/bank/savsi_bank/dao/CustomerDao.java
+++ src/main/java/b7/savsi/foundation/bank/savsi_bank/dao/CustomerDao.java
@@ -2,18 +2,14 @@ package b7.savsi.foundation.bank.savsi_bank.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import b7.savsi.foundation.bank.savsi_bank.entity.Customer;
 
-public class CustomerDao {
+public interface CustomerDao extends JpaRepository<Customer, Integer> {
 
-	
-	public String saveCustomer (Customer customer) {
-		return "";
-	}
-	
-	
-	public List<Customer> findAllCustomers(){
-		
-	}
-	
+	String saveCustomer(Customer customer);
+
+	List<Customer> findAll();
+
 }
